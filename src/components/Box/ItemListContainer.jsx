d925bd49d6b2e70498aa7ae2408029ccbd685650
Gianfranco Geraldo <gianfranco.geraldo@webfactura.cl>
2022-05-05 23:57:58 -0400

    entrega 7

--- src/components/Box/ItemListContainer.jsx
+++ src/components/Box/ItemListContainer.jsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './../../styles/Box/ItemListContainer.css';
-import ItemList from './ItemList';
+import ItemList from '../List/ItemList';
+
+function getProducts(category) {
+  const myPromise = new Promise((resolve, reject) => {
+    const products = [
+      { id: 1, productName: 'Polera', productDescription: 'Polera cuello V', price: 1000, productStock: 3, category: 'Poleras'},
+      { id: 2, productName: 'Bufanda', productDescription: 'Bufando a cuadros', price: 2000, productStock: 7, category: 'temporada' },
+      { id: 3, productName: 'Zapatillas', productDescription: 'Adidas Air Sport',  price: 3000, productStock: 2, category: 'Zapatillas' },
+      
+    ];
+    const productsFiltered = category ? products.filter(product => product.category === category) : products;
+    console.log(productsFiltered);
+    setTimeout(() => {
+      resolve(productsFiltered);
+    }, 2000);
+  });
+  return myPromise;
+}
 
 function ItemListContainer({greeting}) {
 
-  function getProducts() {
-    const myPromise = new Promise((resolve, reject) => {
-      const products = [
-        { id: 1, productName: 'Polera', productDescription: 'Polera cuello V', price: 1000, productStock: 3 },
-        { id: 2, productName: 'Bufanda', productDescription: 'Bufando a cuadros', price: 2000, productStock: 7 },
-        { id: 3, productName: 'Zapatillas', productDescription: 'Adidas Air Sport',  price: 3000, productStock: 2 },
-        
-      ];
-      setTimeout(() => {
-        resolve(products);
-      }, 2000);
-    });
-    return myPromise;
-  }
   const [products, setProducts] = useState([]);
 
+  const { categoryId } = useParams();
+
   useEffect(() => {
-    getProducts()
+    console.log(categoryId);
+    getProducts(categoryId)
       .then(res =>{
         setProducts(res);
       })
-  })
+  }, [categoryId])
 
   return (
     <div className='box'>
